73e3754110bbadc99f8c2b60fa926ac79e4d6a24
dsi <dsi@A0004162>
2022-08-01 12:39:24 +0200

    Person change

--- src/main/java/entities/Person.java
+++ src/main/java/entities/Person.java
@@ -3,9 +3,11 @@ package entities;
 public class Person {
 
 	private int id;
+	private String nom;
 
-	public Person(int id) {
+	public Person(int id, String nom) {
 		super();
 		this.id = id;
+		this.nom = nom;
 	}
 }
